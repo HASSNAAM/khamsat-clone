29671a106ef98532f1956f32e1a0b28c01a652cc
hassnaa <hassnaamahmoud96@gmail.com>
2023-09-13 20:16:48 +0200

    this is khamsat website clone.

---
+++ js/main.js
@@ -0,0 +1,46 @@
+function toggleElement() {
+  var element = document.getElementById("aside");
+
+  if (element.style.display === "none") {
+    element.style.display = "block";
+    element.style.width = "280px";
+  } else {
+    element.style.display = "none";
+  }
+}
+function toggleElement2() {
+  var element = document.getElementById("aside2");
+
+  if (element.style.display === "none") {
+    element.style.display = "block";
+  } else {
+    element.style.display = "none";
+  }
+}
+function toggleElement3() {
+  var element = document.getElementById("aside3");
+
+  if (element.style.display === "none") {
+    element.style.display = "block";
+  } else {
+    element.style.display = "none";
+  }
+}
+function toggleElement4() {
+  var element = document.getElementById("aside4");
+
+  if (element.style.display === "none") {
+    element.style.display = "block";
+  } else {
+    element.style.display = "none";
+  }
+}
+function toggleElement5() {
+  var element = document.getElementById("aside5");
+
+  if (element.style.display === "none") {
+    element.style.display = "block";
+  } else {
+    element.style.display = "none";
+  }
+}
