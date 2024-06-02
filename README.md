1b537cbe955ec14114b591b5c013b92034040176
hassnaa <hassnaamahmoud96@gmail.com>
2024-06-03 00:18:31 +0200

    Edit

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# khamsat-clone
-khamsat clone.
+# Khamsat Clone
+
+Welcome to the **Khamsat Clone**! Because why settle for five when you can have five... more?
+
+## What is this?
+
+This project is a playful imitation of Khamsat, where we take the spirit of freelancing and multiply it by five (or more!).
+
+## Features
+
+- **Multiple Copies of Titles**: Because one title is just not enough to get the message across.
+- **Star Ratings**: Our services are so good, they get rated five stars... five times!
+- **Arabic All the Way**: We're so Arabic, even our images speak the language.
+- **Buttons that Go Nowhere**: Why go somewhere when you can stay here and laugh at the irony?
+
+## Technologies
+
+- **HTML**: The canvas of our quirky masterpiece.
+- **CSS**: Adding colors that make you wonder if we're serious.
+- **Font Awesome**: For when regular stars just won't cut it.
+- **JavaScript**: The magic behind our button that pretends to do something.
+
+## Contact Us
+
+Have questions? Want to tell us how many titles are too many? Send us a message, we'll probably respond with a joke.
+
+- **Github** - [Hassnaa Mahmoud](https://github.com/HASSNAAM)
+- **Frontend Mentor** - [@HASSNAAM](https://www.frontendmentor.io/profile/HASSNAAM)
+- **Email**: [Email](<hassnaamahmoud96@gmail.com>)
+
+## Credits
+
+© hassnaa All rights reserved. Because creativity deserves recognition, even if it's a bit eccentric.
+
+---
+
+*Be unique. Be bold. Be five times as awesome.*
--- js/main.js
+++ js/main.js
@@ -1,46 +1,47 @@
-function toggleElement() {
-  var element = document.getElementById("aside");
+// aside menu
+let bars = document.getElementById("menu-bars");
+let aside = document.getElementById("aside");
 
-  if (element.style.display === "none") {
-    element.style.display = "block";
-    element.style.width = "280px";
+bars.onclick = function () {
+  if (aside.style.display === "block") {
+    aside.style.animationName = "hideAside";
+    setTimeout(() => {
+      aside.style.display = "none";
+    }, 500);
   } else {
-    element.style.display = "none";
+    aside.style.display = "block";
+    aside.style.animationName = "aside";
   }
-}
-function toggleElement2() {
-  var element = document.getElementById("aside2");
-
-  if (element.style.display === "none") {
-    element.style.display = "block";
-  } else {
-    element.style.display = "none";
-  }
-}
-function toggleElement3() {
-  var element = document.getElementById("aside3");
-
-  if (element.style.display === "none") {
-    element.style.display = "block";
-  } else {
-    element.style.display = "none";
-  }
-}
-function toggleElement4() {
-  var element = document.getElementById("aside4");
-
-  if (element.style.display === "none") {
-    element.style.display = "block";
-  } else {
-    element.style.display = "none";
-  }
-}
-function toggleElement5() {
-  var element = document.getElementById("aside5");
-
-  if (element.style.display === "none") {
-    element.style.display = "block";
-  } else {
-    element.style.display = "none";
-  }
-}
+};
+let list1 = document.getElementById("list1");
+let aside2 = document.getElementById("aside2");
+let icon = list1.querySelector(".icon");
+list1.onclick = function () {
+  aside2.classList.toggle("hidden");
+  icon.classList.toggle("fa-angle-left");
+  icon.classList.toggle("fa-angle-down");
+};
+let list2 = document.getElementById("list2");
+let aside3 = document.getElementById("aside3");
+let icon2 = list2.querySelector(".icon2");
+list2.onclick = function () {
+  aside3.classList.toggle("hidden");
+  icon2.classList.toggle("fa-angle-left");
+  icon2.classList.toggle("fa-angle-down");
+};
+let list3 = document.getElementById("list3");
+let aside4 = document.getElementById("aside4");
+let icon3 = list3.querySelector(".icon3");
+list3.onclick = function () {
+  aside4.classList.toggle("hidden");
+  icon3.classList.toggle("fa-angle-left");
+  icon3.classList.toggle("fa-angle-down");
+};
+let list4 = document.getElementById("list4");
+let aside5 = document.getElementById("aside5");
+let icon4 = list4.querySelector(".icon4");
+list4.onclick = function () {
+  aside5.classList.toggle("hidden");
+  icon4.classList.toggle("fa-angle-left");
+  icon4.classList.toggle("fa-angle-down");
+};
